ae82ac29fec3761471e7c80e5dc6a1016b053651
wojtek239 <wojtekopelt1@wp.pl>
2023-11-03 16:47:02 +0100

    fix/test

--- tests/test_encrypting.py
+++ tests/test_encrypting.py
@@ -5,8 +5,7 @@ import pytest
 @pytest.mark.parametrize("text, shift, expected_result", [
     ("world", -3, "tloia"),
 ])
-def test_should_encrypt_word_with_key_negative_shift(self, text, shift,
-                                                     expected_result):
+def test_should_encrypt_word_with_key_negative_shift(text, shift, expected_result):
     try:
         Encryptor(shift).encrypt_word_with_key(text)
     except ValueError as e:
@@ -19,8 +18,7 @@ def test_should_encrypt_word_with_key_negative_shift(self, text, shift,
 @pytest.mark.parametrize("text, shift, expected_result", [
     ("hello", 3, "khoor"),
 ])
-def test_should_encrypt_word_with_key_positive_shift(self, text, shift,
-                                                     expected_result):
+def test_should_encrypt_word_with_key_positive_shift(text, shift, expected_result):
     actual_result = Encryptor(shift).encrypt_word_with_key(text)
     assert actual_result == expected_result
 
@@ -28,8 +26,7 @@ def test_should_encrypt_word_with_key_positive_shift(self, text, shift,
 @pytest.mark.parametrize("text, shift, expected_result", [
     ("abc!123", 2, "cde!123"),
 ])
-def test_should_encrypt_word_with_key_special_characters(self, text, shift,
-                                                     expected_result):
+def test_should_encrypt_word_with_key_special_characters(text, shift, expected_result):
     actual_result = Encryptor(shift).encrypt_word_with_key(text)
     assert actual_result == expected_result
 
@@ -38,8 +35,8 @@ def test_should_encrypt_word_with_key_special_characters(self, text, shift,
     ("To be or not to be, that is the question.", 5,
      "Yt gj tw tsw ts gj, ymfy nx ymj vjxyjtrj."),
 ])
-def test_should_encrypt_word_with_key_spaces_and_punctuation(self, text, shift,
-                                                      expected_result):
+def test_should_encrypt_word_with_key_spaces_and_punctuation(text, shift,
+                                                             expected_result):
     actual_result = Encryptor(shift).encrypt_word_with_key(text)
     assert actual_result == expected_result
 
@@ -47,7 +44,7 @@ def test_should_encrypt_word_with_key_spaces_and_punctuation(self, text, shift,
 @pytest.mark.parametrize("text, shift, expected_result", [
     ("xyz", 3, "abc"),
 ])
-def test_should_encrypt_word_with_key_maximum_shift(self, text, shift, expected_result):
+def test_should_encrypt_word_with_key_maximum_shift(text, shift, expected_result):
     actual_result = Encryptor(shift).encrypt_word_with_key(text)
     assert actual_result == expected_result
 
@@ -55,7 +52,7 @@ def test_should_encrypt_word_with_key_maximum_shift(self, text, shift, expected_
 @pytest.mark.parametrize("text, shift, expected_result", [
     ("aąbcćdeę", 36, "cęgfhijł"),
 ])
-def test_should_encrypt_word_with_key_large_shift(self, text, shift, expected_result):
+def test_should_encrypt_word_with_key_large_shift(text, shift, expected_result):
     actual_result = Encryptor(shift).encrypt_word_with_key(text)
     assert actual_result == expected_result
 
@@ -63,8 +60,7 @@ def test_should_encrypt_word_with_key_large_shift(self, text, shift, expected_re
 @pytest.mark.parametrize("text, shift, expected_result", [
     ("ąęćżźń", 3, "ćęłźźó"),
 ])
-def test_should_encrypt_word_with_key_polish_characters(self, text, shift,
-                                                     expected_result):
+def test_should_encrypt_word_with_key_polish_characters(text, shift, expected_result):
     actual_result = Encryptor(shift).encrypt_word_with_key(text)
     assert actual_result == expected_result
 
@@ -72,6 +68,6 @@ def test_should_encrypt_word_with_key_polish_characters(self, text, shift,
 @pytest.mark.parametrize("text, shift, expected_result", [
     (" ", 3, " "),
 ])
-def test_should_encrypt_word_with_key_empty_string(self, text, shift, expected_result):
+def test_should_encrypt_word_with_key_empty_string(text, shift, expected_result):
     actual_result = Encryptor(shift).encrypt_word_with_key(text)
     assert actual_result == expected_result
